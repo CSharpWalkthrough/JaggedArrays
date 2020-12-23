86d0035c75345af0fb0050146d2901bd5e289a37
sshimabuku <shimasstrafez@gmail.com>
2020-12-23 16:47:36 -0500

    Sample data

--- JaggedArrays/Form1.cs
+++ JaggedArrays/Form1.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace JaggedArrays
 {
@@ -15,6 +7,20 @@ namespace JaggedArrays
         public frmClubMembers()
         {
             InitializeComponent();
+
+            // Initialize jagged array
+            string[][] memberClubs = new string[3][];
+            memberClubs[0] = new string[] { "Jack", "Music", "Tennis", "Chess" };
+            memberClubs[1] = new string[] { "Max", "Tennis" };
+            memberClubs[2] = new string[] { "Alice","Tennis", "Chess", "Basketball", "Soccer" };
+
+            // Display members in lstMembers control
+            foreach (string[] memberClub in memberClubs)
+                lstMembers.Items.Add(memberClub[0]);
+
+            string[] clubs = memberClubs[0];
+            for (int i = 1; i < clubs.Length; i++)
+                lstClubs.Items.Add(clubs[i]);
         }
     }
 }
